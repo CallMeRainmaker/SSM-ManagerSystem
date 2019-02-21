73d3e8b5f699cc0869bf5cad6bfcf908b85c432c
CallMeRainmaker <792087263@qq.com>
2019-02-22 01:53:31 +0800

    完成验证码功能

--- out/artifacts/SSM_ManagerSystem_war_exploded/index.jsp
+++ out/artifacts/SSM_ManagerSystem_war_exploded/index.jsp
@@ -7,5 +7,5 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%
-  response.sendRedirect("system/index");
+  response.sendRedirect("system/login");
 %>
--- src/com/ischoolbar/programmer/controller/SystemController.java
+++ src/com/ischoolbar/programmer/controller/SystemController.java
@@ -1,9 +1,18 @@
 package com.ischoolbar.programmer.controller;
 
+import com.ischoolbar.programmer.util.CpachaUtil;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /*
 系统操作类控制器
@@ -22,5 +31,30 @@ public class SystemController {
         modelAndView.setViewName("index");
         modelAndView.addObject("name","胡旭东");
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public ModelAndView login(ModelAndView modelAndView){
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/get_cpacha",method = RequestMethod.GET)
+    public void generateCpacha(
+            @RequestParam(name = "vl",required = false,defaultValue = "4") Integer vcodeLen,
+            @RequestParam(name = "wd",required = false,defaultValue = "100") Integer width,
+            @RequestParam(name = "ht",required = false,defaultValue = "30") Integer height,
+            @RequestParam(name = "type",required = true,defaultValue = "loginCpacha") String cpachaType,
+            HttpServletRequest request,
+            HttpServletResponse response){
+        CpachaUtil cpachaUtil = new CpachaUtil(vcodeLen,width,height);
+        String generatorVode = cpachaUtil.generatorVCode();
+        request.getSession().setAttribute(cpachaType,generatorVode);
+        BufferedImage generatorRotateVCodeImage = cpachaUtil.generatorRotateVCodeImage(generatorVode,true);
+        try {
+            ImageIO.write(generatorRotateVCodeImage,"gif",response.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
