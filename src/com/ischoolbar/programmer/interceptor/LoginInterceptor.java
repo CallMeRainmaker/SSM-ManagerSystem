2277cee036bb71c2eb49afb2ffba0777c56dd1f8
CallMeRainmaker <792087263@qq.com>
2019-03-04 18:36:53 +0800

    完成拦截器功能

--- src/com/ischoolbar/programmer/interceptor/LoginInterceptor.java
+++ src/com/ischoolbar/programmer/interceptor/LoginInterceptor.java
@@ -1,19 +1,30 @@
 package com.ischoolbar.programmer.interceptor;
 
+import net.sf.json.JSONObject;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        request.getRequestURI();
+        String requestURI = request.getRequestURI();
         Object user = request.getSession().getAttribute("user");
         if(user == null){
-            response.sendRedirect(request.getServletContext().getContextPath()+"/login");
+//            String header = request.getHeader("X-Requested-With");
+//            if("XMLHttpRequest".equals(header)){
+//                Map<String,String> map = new HashMap<>();
+//                map.put("type","error");
+//                map.put("msg","登录会话超时或未登录，请重新登录");
+//                response.getWriter().write(JSONObject.fromObject(map).toString());
+//                return false;
+//            }
+            response.sendRedirect(request.getServletContext().getContextPath()+"/system/login");
             return false;
         }
         return true;
