669c08d59790b15b752ed1df101104a76f5083ec
CallMeRainmaker <792087263@qq.com>
2019-03-11 18:29:53 +0800

    完成权限实体

---
+++ src/com/ischoolbar/programmer/entity/Authority.java
@@ -0,0 +1,34 @@
+package com.ischoolbar.programmer.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Authority {
+    private Long id;
+    private Long roleId;
+    private Long menuId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(Long menuId) {
+        this.menuId = menuId;
+    }
+}
